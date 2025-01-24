d7894f5cf17c99f6074a3e74fb89011e2beed779
Jose Angel Haro Juarez <sanangel010@gmail.com>
2025-01-24 14:22:04 -0600

    Añadir configuración de Webpack y tareas de construcción en package.json

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './preload.js', // Archivo principal
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  target: 'electron-renderer', // Especifica el target para Electron
+};
